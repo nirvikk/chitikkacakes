2d1bc4925068fad6da2faedcc7c9ae8c1901e765
Nirvik Khanal <nirvik.khanal@gmail.com>
2023-09-15 19:23:11 +1000

    Added some bootstrap

--- chitikka-node-service/server.js
+++ chitikka-node-service/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const path = require("path")
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Chitikka Engine!');
+  res.sendFile(path.join(__dirname, "/index.html"))
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
